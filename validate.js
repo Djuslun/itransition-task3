b1f6c7a6706a71262bd19833f0d5c40fff70aadf
Djuslun <djuslun@yandex.ru>
2023-12-05 15:22:26 +0300

    change validate order

--- validate.js
+++ validate.js
@@ -1,15 +1,15 @@
 const validateArguments = (gameArs) => {
   // Возвращаемое значение true/false обозначает прошли аргументы валидацию, или нет
-  if ((gameArs.length % 2) !== 1) {
-    console.log('The number of arguments must be odd')
+  if (gameArs.length <= 1) {
+    console.error('The number of arguments must be more than 1')
     return false
   }
-  if (gameArs.length <= 1) {
-    console.log('The number of arguments must be more than 1')
+  if ((gameArs.length % 2) !== 1) {
+    console.error('The number of arguments must be odd')
     return false
   }
   if (new Set(gameArs).size !== gameArs.length) {
-    console.log('Argгuments must be unique')
+    console.error('Argгuments must be unique')
     return false
   }
 
