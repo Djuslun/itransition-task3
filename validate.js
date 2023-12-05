d763689d5538e0b12351800301415bb70b6c3948
Djuslun <djuslun@yandex.ru>
2023-12-05 15:11:17 +0300

    change validate messages

--- validate.js
+++ validate.js
@@ -1,15 +1,15 @@
 const validateArguments = (gameArs) => {
   // Возвращаемое значение true/false обозначает прошли аргументы валидацию, или нет
   if ((gameArs.length % 2) !== 1) {
-    console.log('Args length must be odd')
+    console.log('The number of arguments must be odd')
     return false
   }
   if (gameArs.length <= 1) {
-    console.log('Args length must be more than 1')
+    console.log('The number of arguments must be more than 1')
     return false
   }
   if (new Set(gameArs).size !== gameArs.length) {
-    console.log('Args must be unique')
+    console.log('Argгuments must be unique')
     return false
   }
 
